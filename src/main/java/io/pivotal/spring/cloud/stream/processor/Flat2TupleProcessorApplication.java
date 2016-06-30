621aa2a00e29959bd60939bb471fb893e7194561
Lei Xu <lxu@pivotal.io>
2016-06-30 20:23:20 +0900

    artifact name changed, todo: to add java -jar mode steps

--- src/main/java/io/pivotal/spring/cloud/stream/processor/Flat2TupleProcessorApplication.java
+++ src/main/java/io/pivotal/spring/cloud/stream/processor/Flat2TupleProcessorApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Flat2TupleTransformerApplication {
+public class Flat2TupleProcessorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Flat2TupleTransformerApplication.class, args);
+		SpringApplication.run(Flat2TupleProcessorApplication.class, args);
 	}
 }
